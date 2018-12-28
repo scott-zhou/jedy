d6e4690baed39fcd9dc59278e89a7c1a4b8613cf
Scott Zhou <scott.cong.zhou@gmail.com>
2018-12-28 23:41:19 +0000

    cont unittest

--- test/test_class_loader.py
+++ test/test_class_loader.py
@@ -1,12 +1,11 @@
 import os
-from unittest import TestCase
-from nose.tools.trivial import ok_, eq_
+import nose
 from lib.class_loader import BootstrapClassLoader
 import logging
 
 
-class TestBootstrapClassLoader(TestCase):
-    def setUp(self):
+class TestBootstrapClassLoader:
+    def setup(self):
         class_loader = BootstrapClassLoader()
         filename = os.path.join(
             os.path.dirname(os.path.realpath(__file__)),
@@ -17,14 +16,17 @@ class TestBootstrapClassLoader(TestCase):
         with open(filename, 'rb') as java_class_file:
             self.class_struct = class_loader.parse(java_class_file)
 
-    def tearDown(self):
+    def teardown(self):
         self.class_struct = None
 
     def test_magic_number_in_classfile_must_be_correct(self):
-        ok_(self.class_struct, 'Load class fail')
-        eq_(self.class_struct.magic, 0xCAFEBABE, 'Magic number wrong')
+        nose.tools.ok_(self.class_struct, 'Load class fail')
+        nose.tools.eq_(
+            self.class_struct.magic, 0xCAFEBABE, 'Magic number wrong')
 
     def test_version_in_classfile_must_be_correct(self):
-        ok_(self.class_struct, 'Load class fail')
-        eq_(self.class_struct.major_version, 52, 'Major version wrong')
-        eq_(self.class_struct.minor_version, 0, 'Minor version wrong')
+        nose.tools.ok_(self.class_struct, 'Load class fail')
+        nose.tools.eq_(
+            self.class_struct.major_version, 52, 'Major version wrong')
+        nose.tools.eq_(
+            self.class_struct.minor_version, 0, 'Minor version wrong')
